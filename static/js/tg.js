ccd06339e31eca9760f841107f45c42eda5bb6e5
vareshinegor <312221301+vareshinegor@users.noreply.github.com>
2026-08-24 11:30:19 +0500

    Revert "Открыть заполнение данных для номинации «Вызов стереотипам»"
    This reverts commit c2847d86c8d246dc20289dfc1aee0102dd53cd07.

--- static/js/tg.js
+++ static/js/tg.js
@@ -440,7 +440,6 @@ window.handleRegNavBtn = function() {
 // ══════════════════════════════════════
 const DATA_PERIODS = {
   spirit:     { start: new Date('2026-08-24'), end: new Date('2026-08-31') },
-  stereo:     { start: new Date('2026-08-24'), end: new Date('2026-08-31') },
   tiresome:   { start: new Date('2026-07-07'), end: new Date('2026-07-16') },
   cristalino: { start: new Date('2026-08-01'), end: new Date('2026-08-11') },
   enlighten:  { start: new Date('2026-08-24'), end: new Date('2026-08-30') },
